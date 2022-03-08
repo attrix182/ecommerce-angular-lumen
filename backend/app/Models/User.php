c4e2741b44726b53669a3b5c1567a81dc6a425e4
luciano <luciano.sinisterra@lamercantil.com.ar>
2022-03-07 21:05:32 -0300

    feature agregue login

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var string[]
      */
     protected $fillable = [
-        'name', 'email',
+        'email',
     ];
 
     /**
@@ -30,4 +30,6 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     protected $hidden = [
         'password',
     ];
+
+    protected $table = "users";
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -31,7 +31,6 @@ $router->put('/products/{id}', 'ProductController@update');
 //Delete products
 $router->delete('/products/{id}', 'ProductController@delete');
 
-
-
-
-
+//Users
+$router->post('/users/register', 'UserController@register');
+$router->post('/users/login', 'UserController@login');
