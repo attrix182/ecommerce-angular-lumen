48a9acea37c97b7e5b91f71f56241a76bb2cc981
luciano <luciano.sinisterra@lamercantil.com.ar>
2022-03-09 19:38:55 -0300

    ordenando componentes

---
+++ frontend/src/app/components/nav/nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'os-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavComponent } from '@app/components/nav/nav.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    NavComponent
   ],
   imports: [
     CommonModule,
   ],
   exports: [
-    HeaderComponent
+    NavComponent
   ]
 })
 export class SharedModule { }
