e6abfc1609b6c4ee5a9e389e6cd87ad6d20fd928
luciano <luciano.sinisterra@lamercantil.com.ar>
2022-03-12 13:35:57 -0300

    downgrade angular - fix navbar

--- frontend/src/app/components/slidebar/slidebar.component.ts
+++ frontend/src/app/components/slidebar/slidebar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-slidebar',
+  selector: 'os-slidebar',
   templateUrl: './slidebar.component.html',
   styleUrls: ['./slidebar.component.scss']
 })
